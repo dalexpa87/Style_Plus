66a2f0e9f1b0a24faedfaa6339c312cc36ec0c2a
RYLOPEZ2 <RYLOPEZ2@MISENA.EDU.CO>
2016-04-21 11:50:09 -0500

    registro

--- views/registrate.php
+++ views/registrate.php
@@ -2,23 +2,25 @@
 <html>
 <head>
 	<meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="recursos\css\estilos_iniciosesion.css">
 	<link href="http://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
       <link type="text/css" rel="stylesheet" href="recursos\plugins\materialize\css\materialize.css"  media="screen,projection"/>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-     <link rel="stylesheet" type="text/css" href="recursos\css\estilos_iniciosesion.css">      
+           
 
 	<title>Registro</title>
 </head>
+<body class="grey lighten-1 center" >  
 
-      <div class="container col s12 l7 m9">
+      <div class="container col s12 l8 m9 ">
 
       <form class="col s12 center" action="../controller/usuarios.controller.php" method="POST">
         <h1 class="center">Regístrate</h1>
 
         <div class="row center" >
-          <div class="col s12  l5 m5 black-text ">
+          <div class="col s12  l5 m5 ">
             <label class="white-text" >Seleccione  tipo de documento </label>
-            <br></br>
+            <br>
             <p>
                 <input name="tipo_documento"  value="CC"type="radio" id="test1" required/ />
                 <label for="test1" class="white-text">Cédula de ciudadania</label>
@@ -39,66 +41,66 @@
           </div> 
 
           <div class="input-field col s10 l5 m5 black-text ">
-            <label>Número de documento</label>
+            <label class="white-text">Número de documento</label>
             <br>
             <input type="text" name="numero_documento" class="validate" required/ >
           </div>
       </div>
       <div class="row" >
-          <div class="input-field col s10 l5 m5 black-text">
-            <label>Asigne Contraseña</label>
-            <br>
-            <input type="password" name="clave" class="validate" required/ >
-          </div>
-
           <div class="input-field col s10 l5 m5 black-text" >
-            <label>Nombres</label>
+            <label class="white-text">Nombres</label>
             <br>
             <input type="text" name="nombre" class="validate" required/ >
+          </div>
+          <div class="input-field col s10 l5 m5 black-text">
+            <label class="white-text">Apellidos</label>
+            <br>
+            <input type="text" name="apellido" class="validate"  >
           </div>
       </div>
       <div class="row" >
           <div class="input-field col s10 l5 m5 black-text">
-            <label>Apellidos</label>
+            <label class="white-text">Número celular</label>
             <br>
-            <input type="text" name="apellido" class="validate"  >
-          </div>
+            <input type="text" name="celular" class="validate" required/>
+          </div> 
 
           <div class="input-field col s10 l5 m5 black-text">
-            <label>Número de telefono</label>
+            <label class="white-text">Número de telefono</label>
             <br>
             <input type="text" name="telefono" class="validate" required/ >
           </div>
       </div>
       <div class="row" >
           <div class="input-field col s10 l5 m5 black-text">
-            <label>Dirección</label>
-            <br></br>
+            <label class="white-text">Dirección</label>
+            <br>
             <input type="text" name="direccion" class="validate" required/ >
           </div>
 
           <div class="input-field col s10 l5 m5 black-text">
-            <label>Ciudad</label>
+            <label class="white-text">Ciudad</label>
             <br>
             <input type="text" name="ciudad" class="validate" required/ >
           </div>
       </div>
       <div class="row" >
           <div class="input-field col s10 l5 m5 black-text">
-            <label>Correo Electrónico</label>
+            <label class="white-text">Correo Electrónico</label>
             <br>
             <input type="email" name="correo" class="validate" required/ >
           </div>
-
           <div class="input-field col s10 l5 m5 black-text">
-            <label>Número celular</label>
+            <label class="white-text">Asigne una  contraseña</label>
             <br>
-            <input type="text" name="celular" class="validate" required/>
+            <input type="password" name="clave" class="validate" required/>
           </div>
+
+
       </div>
       <div class="row" >
           <div class="input-field col s10 l5 m5 black-text">
-            <label>fecha de nacimiento </label>
+            <label class="white-text">fecha de nacimiento </label>
             <br>
             <input type="date" name="fecha_nacimiento" min="1900-01-01" class="validate" required/ >
           </div>
@@ -109,34 +111,31 @@
             <p>
                 <input name="sexo"  value="mujer"type="radio" id="sex1" required/ />
                 <label for="sex1" class="white-text">Femenino</label>
-              </p>
+            </p>
               <p>
                 <input name="sexo"  value="Hombre" type="radio" id="sex2" />
                 <label for="sex2" class="white-text">masculino</label>
               </p>
-              
-
           </div> 
       </div> 
-      <input type="hidden" name="estado" value="Activo">
-      <input type="hidden" name="id_rol" value="1">
-        
-      </div>
-      <input type="hidden" name="autor" value="Autoregistrado">               
-         <button  type="botton" name="acc" value="c" class="waves-effect black btn"> Enviar</button>
-         <button type="button" class="waves-effect black btn"><a href="iniciosesion.php">cancelar</a></button>
+      <input type="hidden" name="estado" value="1">
+      <input type="hidden" name="id_rol" value="1">      
+      <input type="hidden" name="autor" value="Autoregistrado">
 
-    
-      </form>
-      </div>
-    </div>
-    
+      <button  type="botton" name="acc" value="c" class="waves-effect black btn"> Enviar</button>
+      <button type="button" class="waves-effect black btn"><a href="index.php" class="white-text" >cancelar</a></button>
+
+  </form> 
   </div>
-<body class="grey lighten-1">      
-  
-  
-        
-   <script type="text/javascript" src="jquery-1.12.1.min.js"></script>
+  <?php
+                  if( base64_decode(@$_GET["tm"]) == "advertencia"){
+                    $estilos = "orange";
+                  }else{
+                    $estilos = "red";
+                  }
+
+                  echo "<div style='background-color:".$estilos."'>".base64_decode(@$_GET["m"])."</div>";?>
+  <script type="text/javascript" src="jquery-1.12.1.min.js"></script>
  	<script type="text/javascript" src="materialize\js\materialize.min.js"></script>
    <script type="text/javascript">
     $('.datepicker').pickadate({
